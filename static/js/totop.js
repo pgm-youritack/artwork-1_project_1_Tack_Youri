f7968557cbb27a905ade4bcfa7b296fb319845b3
pgm-youritack <youri.tack@student.arteveldehs.be>
2021-11-23 15:49:41 +0100

    3rde push

--- static/js/totop.js
+++ static/js/totop.js
@@ -1,18 +1,19 @@
-//Get the button
+//top variable
 const naartopknop = document.getElementById("top");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// als men scrollt 20px naar beneden 
+window.onscroll =function(){scrollFunction()}
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    naartopknop.style.display = "block";
+  if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+      naartopknop.style.display = 'block';
+      naartopknop.style.animation= '    fadein 450ms';
   } else {
-    naartopknop.style.display = "none";
+      naartopknop.style.display = 'none';
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// wanneer het persoon op de back to top klikt
 function topFunction(){
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
